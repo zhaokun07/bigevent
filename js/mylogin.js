2275578f749209a127eafe9b7ca195e442988742
itheima <itheima@itheima.cn>
2022-01-12 16:23:28 +0800

    16:23

--- js/mylogin.js
+++ js/mylogin.js
@@ -1,9 +1,14 @@
-$('.goRegister').on('click',()=>{
+$('.goRegister').on('click', () => {
     $('.form-register').show()
     $('.form-login').hide()
 })
 
-$('.goLogin').on('click',()=>{
+$('.goLogin').on('click', () => {
     $('.form-login').show()
     $('.form-register').hide()
 })
+
+// let form = layui.form
+// form.verify({
+//     pwd:[/^[\S]{6,12}$/,'密码必须6-12位，且不能出现空格']
+// })

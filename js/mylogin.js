cad9f1bd5e05b71e41b63fed641e10629f6951ba
itheima <itheima@itheima.cn>
2022-01-12 15:21:39 +0800

    15:21

--- js/mylogin.js
+++ js/mylogin.js
@@ -1,9 +1,9 @@
-$('.enter').on('click', () => {
-    if ($('.enter').html() == '去注册账号') {
-        $('.register-pwd2').show()
-        $('.enter').html('去登录')
-    } else {
-        $('.register-pwd2').hide()
-        $('.enter').html('去注册账号')
-    }
+$('.goRegister').on('click',()=>{
+    $('.form-register').show()
+    $('.form-login').hide()
+})
+
+$('.goLogin').on('click',()=>{
+    $('.form-login').show()
+    $('.form-register').hide()
 })

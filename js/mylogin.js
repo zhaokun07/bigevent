020c50db1c1551c4644ef911c6f9043ab830e2e7
itheima <itheima@itheima.cn>
2022-01-16 21:29:31 +0800

    1.16.21.30

--- js/mylogin.js
+++ js/mylogin.js
@@ -9,6 +9,7 @@ $('.goLogin').on('click', () => {
 })
 
 let form = layui.form
+let layer = layui.layer
 form.verify({
     pwd: [/^[\S]{6,12}$/, '密码必须6-12位，且不能出现空格'],
     rePwd: function (value) {
@@ -19,13 +20,35 @@ form.verify({
 
 $('.form-register').on('submit', (e) => {
     e.preventDefault()
-    console.log(1111);
     $.post(
-        'http://ajax.frontend.itheima.net/api/reguser', {
+        'http://www.liulongbin.top:3007/api/reguser', {
             username: $('.form-register [name=username]').val(),
             password: $('.form-register [name=password]').val()
         }, res => {
             if (res.status !== 0) return console.log(res.message);
-            console.log('注册成功');
+            console.log('注册成功')
+            layer.msg('注册成功')
+            $('.goLogin').click()
         })
+})
+
+$('.form-login').on('submit', (e) => {
+    console.log($('.form-login').serialize());
+    e.preventDefault()
+    $.ajax({
+        method: 'POST',
+        url: 'http://www.liulongbin.top:3007/api/login',
+        data: {
+            username: $('.form-login [name=username]').val(),
+            password: $('.form-login [name=password]').val()
+        },
+        success: res => {
+            console.log(res.token);
+            location.href = 'index.html'
+            localStorage.setItem('token',res.token)
+        }
+
+
+
+    })
 })
